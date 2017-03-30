a23f79987e2a473f2197ed35dae43c11b3033e1a
wangqingsong <wangqs@cloud-young.com>
2017-03-30 11:53:33 +0800

    简单的操作日历提醒

--- EventCalendarDemo/ViewController.m
+++ EventCalendarDemo/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "EventCalendar.h"
 
 @interface ViewController ()
 
@@ -24,6 +25,30 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+//自定义事件事件，自动添加到日历
+- (IBAction)testAction:(id)sender {
+    
+    
+    EventCalendar *event = [EventCalendar sharedEventCalendar];
+    NSDate *date = [NSDate date];
+    [event createEventCalendarTitle:@"测试" location:@"公司" startDate:date endDate:date allDay:YES alarmArray:@[@(-7.f),@(-17.f)]];
+    
+ 
+}
+//跳转到系统日历
+- (IBAction)sysTestAction:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"calshow://"];
+    [[UIApplication sharedApplication] openURL:url options:nil completionHandler:^(BOOL success) {
+        
+    }];
+}
+//跳转到系统提醒事项
+- (IBAction)tiIXingAction:(id)sender {
+    NSURL *url = [NSURL URLWithString:@"x-apple-reminder://"];
+    [[UIApplication sharedApplication] openURL:url options:nil completionHandler:^(BOOL success) {
+        
+    }];
 
+}
 
 @end
